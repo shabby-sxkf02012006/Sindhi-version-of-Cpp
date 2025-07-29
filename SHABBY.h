4d40323ad2822f80f843161edc7c1367e9bf1a96
Shahbaz Ali <shahbazali078272@gmail.com>
2025-07-29 14:39:44 +0500

    Add files via upload

---
+++ SHABBY.h
@@ -0,0 +1,24 @@
+#ifndef SHABBY
+#define SHABBY
+#include <iostream>
+//Normal Statements like Output and input
+#define Budh  std::cin
+#define Budhae  std::cout
+#define NaeLineWath std::endl
+//Conditional Statements
+#define Jekadhen if
+#define NaTaPoe else
+#define Agar switch
+#define SoratEHal case
+#define BaakiSB defualt
+//Datatypes
+#define BegairPoint int
+#define WadoNumber double
+#define PointWaro float
+#define Lafz string
+#define Akhar char
+// Symbols
+#define barabarNahy !=
+#define barabarAahy ==
+#define Maalik "\n\n*********************************************\nThis Sindhi Version of C++ is created By shabby.\n He is currently studing in Department of Information Tecchnoloogya at University of Sindh Jamshoro.\nFor Contribution and support of this work\nDo check it out with this link: https://portfolio-ebfshahbaz01.netlify.app\n*********************************************\n \n "
+#endif // SHABBY
